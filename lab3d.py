60c8683c817fc0e4b5909ec4000b93cc12271fda
Geetansh Chawla <gchawla4@myseneca.ca>
2024-05-31 16:18:46 -0400

    added lab3d

--- lab3d.py
+++ lab3d.py
@@ -5,15 +5,15 @@
 import subprocess
 
 def free_space():
-    # Launch the Linux command: df -h | grep '/$' | awk '{print $4}' as a new process
+ 
     p = subprocess.Popen(['df', '-h'], stdout=subprocess.PIPE)
     p1 = subprocess.Popen(['grep', '/$'], stdin=p.stdout, stdout=subprocess.PIPE)
     p2 = subprocess.Popen(['awk', '{print $4}'], stdin=p1.stdout, stdout=subprocess.PIPE)
     
-    # Retrieve output
+    
     output = p2.communicate()[0]
     
-    # Convert stdout to a string and strip off the newline characters
+    
     free_space = output.decode('utf-8').strip()
     
     return free_space
